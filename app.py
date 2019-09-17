df0a374170d83950f9b2ee307cc1ceeb1d4210cf
Ubuntu <ubuntu@ip-172-31-87-173.ec2.internal>
2019-09-17 14:00:14 +0000

    bad auth errors resolved, linked base and recipe html

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os 
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template , redirect , request , url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
 
 app = Flask(__name__)
+
 app.config["MONGO_DBNAME"] = 'drinks'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
@@ -14,13 +15,10 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_recipes')
-def get_tasks():
+def get_recipes():
     return render_template("recipes.html", drinks_list=mongo.db.drinks_list.find())
     
 
-    
-    
-    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
