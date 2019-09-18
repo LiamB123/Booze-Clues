b5204f92ed1702da71f5706ebfdb3ff503047f80
Ubuntu <ubuntu@ip-172-31-87-173.ec2.internal>
2019-09-18 16:39:41 +0000

    Fixed issues with database communicating

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
-
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'drinks'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
