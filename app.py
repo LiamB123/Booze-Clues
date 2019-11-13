9ce2cef1beaad4b7e69e9c2fecbbbef9cebd7778
Ubuntu <ubuntu@ip-172-31-87-173.ec2.internal>
2019-11-13 17:30:32 +0000

    debug turned off

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def find_shop():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
